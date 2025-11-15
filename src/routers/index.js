a593f80c31422075b6b3607974f104827f69e74f
SotnikovSS <sotnikovss90@gmail.com>
2025-11-15 16:20:11 +0100

    go

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import authRouter from './auth.js';
 import stories from './stories.js';
 import users from './users.js';
+import categories from './categories.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/stories', stories);
 router.use('/users', users);
+router.use('/categories', categories);
 export default router;
